97c65ccaf81b4c129f7fc5dc885a8ae2369bcdc2
devmeeple <devmeeple@gmail.com>
2023-06-25 20:14:46 +0900

    PRG Post/Redirect/Get

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -71,10 +71,17 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV4(Item item) {
         itemRepository.save(item);
         return "basic/item";
+    }
+
+//    PRG 패턴
+    @PostMapping("/add")
+    public String addItemV5(Item item) {
+        itemRepository.save(item);
+        return "redirect:/basic/items/" + item.getId();
     }
 
     @GetMapping ("/{itemId}/edit")
